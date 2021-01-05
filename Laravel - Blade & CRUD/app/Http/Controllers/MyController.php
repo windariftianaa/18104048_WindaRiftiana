fe53caeac1894ce8a3f38ed11078beca63b0be77
windariftianaa <18104048@ittelkom-pwt.ac.id>
2021-01-05 06:47:22 -0900

    Laravel - Blade & CRUD

---
+++ Laravel - Blade & CRUD/app/Http/Controllers/MyController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MyController extends Controller
+{
+    //
+    public function index(){
+        return view('beranda');
+    }
+
+    public function about(){
+        return view('about');
+    }
+}
